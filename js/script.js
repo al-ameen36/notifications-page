a4d17c2ecf8b6907450354eaf0a838d6905281cc
Muhammad Al-Ameen Abdullahi <muhdabdullahi36@gmail.com>
2022-10-16 19:41:40 +0100

    add individual mark as read funtionality  & notification count funtionality

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
+let notification_count = document.getElementById('notification-count')
+notification_count.innerHTML = document.getElementsByClassName('unread').length
+
 let readAll_btn = document.getElementById('readAll-btn');
 readAll_btn.addEventListener('click', () => {
     Array.from(document.getElementsByClassName('notification')).forEach((el) => el.classList.remove('unread'))
+    notification_count.innerHTML = document.getElementsByClassName('unread').length
+})
+
+let unread_notifications = document.getElementsByClassName('unread')
+Array.from(unread_notifications).forEach((unread) => {
+    unread.addEventListener('click', function () {
+        this.classList.remove('unread')
+        notification_count.innerHTML = document.getElementsByClassName('unread').length
+    })
 })
